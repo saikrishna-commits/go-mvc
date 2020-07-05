e7157f0e9281be942b3683712a9c2abd755dba6c
cyberbot143 <krishnasai952@gmail.com>
2020-07-04 20:54:53 -0400

    movie model

---
+++ graph/model/movie.go
@@ -0,0 +1,20 @@
+package model
+
+
+type ImdbRating struct {
+	Rating float64 `json:"rating,omitempty"`
+	Votes  int64   `json:"votes"`
+}
+
+type Movie struct {
+	_id         string     `json:"id,omitempty" bson:"_id"`
+	Title       string     `json:"title,omitempty"json:"title"`
+	Cast        []string   `json:"cast,omitempty"`
+	Directors   []string   `json:"directors"`
+	Genres      []string   `json:"genres,omitempty"`
+	Year        int64      `json:"year,omitempty"`
+    // Released    time.Time  `json:"released,omitempty"`
+    //  LastUpdated time.Time  `json:"lastUpdated,omitempty"`
+	ImdbRating  ImdbRating `json:"imdb"`
+}
+
